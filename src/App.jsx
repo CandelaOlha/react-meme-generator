ffa2a8098b2ba8add2140f9be59916a454990aba
“c.b.olha” <c.b.olha@viseven.com>
2024-02-05 11:45:51 -0300

    project setup/added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1>React Meme Generator</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "../styles/Header.css";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-title-container">
+        <img src={logo} alt="" className="header-image" />
+        <h1 className="header-title">Meme Generator</h1>
+      </div>
+      <h2 className="project-description">React Course - Project 3</h2>
+    </header>
+  );
+};
+
+export default Header;
